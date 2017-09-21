7f111daff2f627183dd7842a0ad764737b2500e2
Christopher Holland <chris@tophware.com>
2017-09-20 21:13:05 -0400

    Adding another class?

---
+++ Classes/TophLib.swift
@@ -0,0 +1,16 @@
+//
+//  TophLib.swift
+//  Pods-TophLib_Example
+//
+//  Created by Christopher Holland on 9/20/17.
+//
+
+import Foundation
+
+open class TophLib {
+
+    public init() {
+        print("Init TophLib")
+    }
+    
+}
